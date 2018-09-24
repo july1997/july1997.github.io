755e1d35d380755c425e452f3e8136376b4304f5
july1997 <kamimamita@aol.jp>
2018-09-25 02:34:29 +0900

    サイト更新 メールフォーム修正

--- js/mail.js
+++ js/mail.js
@@ -3,7 +3,7 @@ const CLIENT_ID = '361625052892-1u48d2n9trgmu5lvc7se3tku6dj45t5l.apps.googleuser
 
 async function onLoad() {
   try {
-    $('#step1').hide();
+    $('#step2').hide();
 
     // Google APIs Client Libraryの初期化。
     await gapi.load('client:auth2');
