32ef9faf73ac86e552729d589e93fc61828acc53
july1997 <kamimamita@aol.jp>
2018-09-27 03:59:26 +0900

    ニュース情報を外部ファイル化

--- js/script.js
+++ js/script.js
@@ -44,6 +44,42 @@ $(function () {
     });
   });
 
+  // ニュースの読み込み
+  $(function () {
+    $.ajax({
+      url:'news.txt',
+      success: function(data){
+        var data_array = data.split(/\r\n|\r|\n/);  // 改行コードで分割
+        var len = data_array.length;
+        if(len > 36)len = 36;
+        var rowlengh = 6;
+        for (var i = 0; i < len; i++){
+          switch(i % rowlengh){
+            case 0:
+              $("#newscolums").append('<div class="card border-' + data_array[i] + '"><div class="card-body"' + 'id=colums' + Math.floor(i/rowlengh) +
+               '><h4 class="card-title text-' + data_array[i] + '"' + 'id=columstitle' + Math.floor(i/rowlengh) + '>');
+            break;
+            case 1:
+              $("#columstitle" + Math.floor(i/rowlengh)).append(data_array[i]);
+            break;
+            case 2:
+              $("#colums" + Math.floor(i/rowlengh)).append('<p class="card-text">' + data_array[i]);
+            break;
+            case 3:
+              $("#colums" + Math.floor(i/rowlengh)).append('<a href="' + data_array[i] + '" class="card-link" ' + 'id=columslink' + Math.floor(i/rowlengh) + '>');
+            break;
+            case 4:
+              $("#columslink" + Math.floor(i/rowlengh)).append(data_array[i] + "</a>");
+            break;
+            case 5:
+              $("#colums" + Math.floor(i/rowlengh)).append("<p class=\"card-text\"><small class=\"text-muted\">" + data_array[i] + "</small></p>");
+            break;
+          }
+        }
+      }
+    });
+  });
+
   //ボタンの処理
 $(function() {
   $('#btn-all').click(function(){
